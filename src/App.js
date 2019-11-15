9224c979915222cee58930a289b6f556ca589d87
Robert Gordon <Komeuppance@gmail.com>
2019-11-15 08:12:58 -0800

    Routing setup, navlinks added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage"
-
-
+import CharacterList from "./components/CharacterList";
+import LocationsList from "./components/LocationsList";
 
 export default function App() {
   return (
     <main>
       <Header />
       <Route exact path='/' component={WelcomePage}></Route>
+      <Route path='/characters-list' component={CharacterList}></Route>
+      <Route path='/locations-list' component={LocationsList}></Route>
     </main>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
+import styled from 'styled-components';
+
+const NavLinks = styled.div`
+  display: flex;
+  justify-content: space-around;
+
+`
 
 export default function Header() {
   return (
+    <>
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
     </header>
+    <NavLinks>
+      <NavLink exact to="/">Home</NavLink>
+      <NavLink to="/characters-list">Characters</NavLink>
+      <NavLink to="/locations-list">Locations</NavLink>
+    </NavLinks>
+    </>
   );
 }
