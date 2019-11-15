ad0c5fe52a20de5cb7b8532facb9d3c613093113
Robert Gordon <Komeuppance@gmail.com>
2019-11-15 08:28:40 -0800

    Characters list working, added css styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,40 @@
 import React from "react";
+import styled from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Card =  styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  /* align-items: center; */
+  border: 1px solid black;
+  border-radius: 5px;
+  width: 250px;
+  height: 250px;
+  margin: 20px;
+  padding: 15px;
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+  color: white;
+  text-shadow: 2px 2px 4px #000000;
+  background-image: url('https://cdn.clipart.email/a09286f3801b6184b2c79e37f0dc6fe8_rick-and-morty-portal-rick-and-morty-t-shirt-teepublic_630-630.jpeg');
+  background-size: cover;
+  div{
+    margin: 3px 0;
+  }
+  span{
+    font-weight: bold;
+  }
+`
+
+
+export default function CharacterCard(props) {
+  return (
+    <>
+    <Card>
+      <div><span>Name:</span> {props.name}</div>
+      <div><span>Species:</span> {props.species}</div>
+      <div><span>Status:</span> {props.status}</div>
+      <div><span>Location:</span> {props.location.name}</div>
+    </Card>
+  </>
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,42 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+import styled from 'styled-components';
+
+const CharacterCards = styled.section`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  max-height: 750px;
+  overflow: auto;
+`
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios
+      .get(`https://rickandmortyapi.com/api/character`)
+      .then(response => {
+        console.log(response.data.results)
+        setCharacters(response.data.results)
+      })
+      .catch (error => {
+        console.log (error);
+      })
+
+  },[]);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <>
+      <CharacterCards>
+      {characters.map (character => (
+        <CharacterCard key={character.id} name={character.name} species={character.species} status={character.status} location={character.location}/>
+      ))}
+    </CharacterCards>
+    </>
   );
 }
