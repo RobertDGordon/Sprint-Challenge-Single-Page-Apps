8c974d5d4316149b5bb731c596f97eec9345fb91
Robert Gordon <Komeuppance@gmail.com>
2019-11-15 08:15:25 -0800

    Routing working to characters and locations

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,10 @@
 import React, { useEffect, useState } from "react";
 
-export default function LocationsList() {}
+export default function LocationsList() {
+
+    return (
+        <section className="character-list">
+        <h2>Locations</h2>
+        </section>
+    );
+}
