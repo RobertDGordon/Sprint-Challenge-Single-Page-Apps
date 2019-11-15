92930c91bec3c16fe2c54304309b3f10a535fe6a
Robert Gordon <Komeuppance@gmail.com>
2019-11-15 08:09:13 -0800

    Welcome page setup

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,15 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const Head = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
-        <h1>Welcome to the ultimate fan site!</h1>
+      <Head>
+        <h1>Let's get schwifty!</h1>
         <img
           className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+          src="https://static.lootcrate.com/yrk2x9n6p9s5/7qe1Eupm4acN9PffLD5WLw/3586f3356d4526cfd964b8dfed7f9887/slide-1-OptA.gif"
           alt="rick"
         />
+        </Head>
       </header>
     </section>
   );
